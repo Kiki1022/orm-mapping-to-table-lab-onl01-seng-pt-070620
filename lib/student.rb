3e3889320c8840d619335e6ebccbfb4736406ce4
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-10 18:02:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,15 @@
 class Student
+  
+  attr_reader :id
+  
+  
+  
+  
+  
+  
+  
+end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-end
