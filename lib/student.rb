3c4a2975454cd9550e685aaeeebf46c46087acff
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-10 18:07:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,11 @@ class Student
   attr_reader :id 
   attr_accessor :name, :grade
   
-  # def initialize
-  # end
+  def initialize (name, grade, id=nil)
+    @id = id 
+    @name = name
+    @grade = grade
+  end
   
   
   
